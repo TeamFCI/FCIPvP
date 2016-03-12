7cb1c9dd874a8e7b12de6c5a16c9947412341daa
ArianoMiguel <ArianoMiguel@FreakPuter>
2016-03-12 21:27:15 +0100

    Commands Added EffectLib Added

--- FCI PVP-System/src/de/happyhappyboy/commands/CommandsFcipvp.java
+++ FCI PVP-System/src/de/happyhappyboy/commands/CommandsFcipvp.java
@@ -1,5 +1,43 @@
 package de.happyhappyboy.commands;
 
-public class CommandsFcipvp {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import de.happyhappyboy.main.FCIPvP;
+import de.slikey.effectlib.Effect;
+import de.slikey.effectlib.EffectManager;
+
+public class CommandsFcipvp implements CommandExecutor {
+	EffectManager em = FCIPvP.em;
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(sender instanceof Player) {
+			Player p = (Player) sender; 
+			if(args.length == 0) {
+				if(p.hasPermission("fci.fcipvp.fcipvp")) {
+					p.sendMessage("§b/fcipvp helix");
+					p.sendMessage("§b/fcipvp dna");
+					p.sendMessage("§b/fcipvp atom");
+				} else {
+					p.sendMessage("§cFehler: Du hast nicht die Permission dazu!");
+				}
+			}
+			if(args.length == 1) {
+				if(p.hasPermission("fci.fcipvp.example")) {
+					
+					Effect eff = new Ef(em);
+					
+				} else {
+					p.sendMessage("§cFehler: Du hast nicht die Permission dazu!");
+				}
+			}
+		}
+		
+		return false;
+	}
+	
 }
--- FCI PVP-System/src/de/happyhappyboy/main/FCIPvP.java
+++ FCI PVP-System/src/de/happyhappyboy/main/FCIPvP.java
@@ -3,11 +3,13 @@ package de.happyhappyboy.main;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.slikey.effectlib.EffectManager;
+
 public class FCIPvP extends JavaPlugin {
-	
+	public static EffectManager em;
 	public void onEnable() {
 		PluginManager Pm = this.getServer().getPluginManager();
-		
+		em = new EffectManager(this);
 		
 		
 	}
