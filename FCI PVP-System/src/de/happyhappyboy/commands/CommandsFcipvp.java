ccbc20b33de8b539b2580aa8c697656b811fd87f
ArianoMiguel <ArianoMiguel@FreakPuter>
2016-03-12 20:59:24 +0100

    Repos. Angelegt!

---
+++ FCI PVP-System/src/de/happyhappyboy/commands/CommandsFcipvp.java
@@ -0,0 +1,5 @@
+package de.happyhappyboy.commands;
+
+public class CommandsFcipvp {
+
+}
---
+++ FCI PVP-System/src/de/happyhappyboy/main/FCIPvP.java
@@ -0,0 +1,15 @@
+package de.happyhappyboy.main;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FCIPvP extends JavaPlugin {
+	
+	public void onEnable() {
+		PluginManager Pm = this.getServer().getPluginManager();
+		
+		
+		
+	}
+	
+}
